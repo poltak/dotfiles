10edfe75edf48f0c2957929918098c185fed3753
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2013-12-23 05:50:16 +1100

    Updated aliases to be more robust
    Changed hard-coded absolute paths to simpler 'open -a [app-name]'
    commands

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias ....='../../..'
 alias update='brew -v update && brew upgrade'
 
 # Mac OS X application aliases for use in shell.
-alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
-alias safari='open -a /Applications/Safari.app'
-alias subl='open -a /Applications/Sublime\ Text\ 2.app'
-alias preview='open -a /Applications/Preview.app'
+alias firefox='open -a "Firefox"'
+alias safari='open -a "Safari"'
+alias subl='open -a "Sublime Text 2"'
+alias preview='open -a "Preview"'
