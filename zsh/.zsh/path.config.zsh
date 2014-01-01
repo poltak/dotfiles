73b4ae81b8c1046c84e1d0d457b8a09ba8005268
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-01-01 00:29:14 -0800

    Made Z shell path config nicer and more extensible

--- zsh/.zsh/path.config.zsh
+++ zsh/.zsh/path.config.zsh
@@ -1,3 +1,17 @@
 # Custom path additions.
+
+# Where my own scripts are synced.
+DROPBOX_BIN=($HOME/Dropbox/bin)
+
+# Where mactex puts its bin.
+LATEX_BIN=(/usr/local/texlive/2013/bin/universal-darwin)
+
+# Where homebrew puts its bins.
+HOMEBREW_BIN=(/usr/local/bin /usr/local/sbin)
+
+# Where OS X puts its bins.
+SYSTEM_BIN=(/bin /usr/bin /sbin /usr/sbin)
+
+# Actually set the path in Z shell.
 typeset -U path
-path=($HOME/Dropbox/bin /usr/local/bin /usr/local/sbin /bin /usr/sbin /sbin /usr/bin) #$path)
+path=($DROPBOX_BIN $LATEX_BIN $HOMEBREW_BIN $SYSTEM_BIN)
