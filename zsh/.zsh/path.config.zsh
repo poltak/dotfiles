27c8efab7713311f3dea6a39bc94749b470b649d
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-01-04 20:48:58 -0800

    Added android-sdk platform-tools to zsh path
    Also updated .rtorrent.rc for use on OS X.

--- zsh/.zsh/path.config.zsh
+++ zsh/.zsh/path.config.zsh
@@ -12,6 +12,9 @@ HOMEBREW_BIN=(/usr/local/bin /usr/local/sbin)
 # Where OS X puts its bins.
 SYSTEM_BIN=(/bin /usr/bin /sbin /usr/sbin)
 
+# Where android-sdk platform-tools are installed.
+ANDROID_SDK_BIN=($HOME/Library/Developer/Xamarin/android-sdk-mac_x86/platform-tools)
+
 # Actually set the path in Z shell.
 typeset -U path
-path=($DROPBOX_BIN $LATEX_BIN $HOMEBREW_BIN $SYSTEM_BIN)
+path=($DROPBOX_BIN $LATEX_BIN $HOMEBREW_BIN $SYSTEM_BIN $ANDROID_SDK_BIN)
