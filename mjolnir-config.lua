df591ca9d44615db1b8cd106a0f24324b6888aa3
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-09-27 20:46:16 +1000

    Removed sleep functionality from mjolnir
    - it was buggy as hell

--- mjolnir-config.lua
+++ mjolnir-config.lua
@@ -1,6 +1,5 @@
 local app      = require "mjolnir.application"
 local hotkey   = require "mjolnir.hotkey"
-local sleep    = require "mjolnir.cmsj.caffeinate"
 local alert    = require "mjolnir.alert"
 local spotify  = require "mjolnir.lb.spotify"
 
@@ -32,16 +31,3 @@ hotkey.bind({"alt"}, "n", function()
   spotify.displayCurrentTrack()
 end)
 
-
-
--- Caffeine and NoSleep functionality
-
-sleep.set("SystemIdle", True, True)
-
-hotkey.bind({"alt"}, "z", function()
-  if sleep.toggle("System") then
-    alert.show("System sleep disabled")
-  else
-    alert.show("System sleep enabled")
-  end
-end)
